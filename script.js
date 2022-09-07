d9dd41e10f709f131689bf8f4788ed93e4bd8666
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-09-07 16:38:03 -0300

    Implementei a função para manter a paleta de cores salva no localStorage

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 const secaoPaletaCores = document.getElementById('color-palette');
 const divPaletaCores = document.getElementsByClassName('color');
 const botaoCores = document.getElementById('button-random-color');
+const divCor2 = document.getElementById('cor2');
+const divCor3 = document.getElementById('cor3');
+const divCor4 = document.getElementById('cor4');
 
-console.log(secaoPaletaCores);
-console.log(divPaletaCores);
-console.log(botaoCores);
+// console.log(secaoPaletaCores);
+// console.log(divPaletaCores);
+// console.log(botaoCores);
+// console.log(divCor2);
+
 
 function geradorDeCores() {
- let cor2 = parseInt(Math.random() * 255);
- let cor3 = parseInt(Math.random() * 255);
- let cor4 = parseInt(Math.random() * 255);
- let corFinal = 'rgb(' + cor2 + ', ' + cor3 + ', ' + cor4 + ')';
- return corFinal;
+  const cor2 = parseInt(Math.random() * 255);
+  const cor3 = parseInt(Math.random() * 255);
+  const cor4 = parseInt(Math.random() * 255);
+  const corFinal = 'rgb(' + cor2 + ', ' + cor3 + ', ' + cor4 + ')';
+  return corFinal;
 }
 
 function coresAleatorias() {
- for (let index = 1; index < divPaletaCores.length; index += 1) {
-  divPaletaCores[index].style.backgroundColor = geradorDeCores();
- }
+  for (let index = 1; index < divPaletaCores.length; index += 1) {
+    divPaletaCores[index].style.backgroundColor = geradorDeCores();
+  }
+  let paletaCompleta = {
+    paleta2: divCor2.style.backgroundColor,
+    paleta3: divCor3.style.backgroundColor,
+    paleta4: divCor4.style.backgroundColor
+   };
+  //console.log(paletaCompleta);
+  localStorage.setItem('colorPalette', JSON.stringify(paletaCompleta));
 }
 
-botaoCores.addEventListener('click', coresAleatorias);
+const botaoDoClick = botaoCores.addEventListener('click', coresAleatorias);
+let paletaSalva = JSON.parse(localStorage.getItem('colorPalette'));
+//console.log (paletaSalva);
+if (paletaSalva !== null) {
+  divCor2.style.backgroundColor = paletaSalva.paleta2;
+  divCor3.style.backgroundColor = paletaSalva.paleta3;
+  divCor4.style.backgroundColor = paletaSalva.paleta4;
+}
+
+
+
