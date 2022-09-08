7f7d4b85a4e5a8bf31a393c4b45376a6e73c2a1c
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-09-07 22:14:41 -0300

    Criei um quadro contendo 25 pixels

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const botaoCores = document.getElementById('button-random-color');
 const divCor2 = document.getElementById('cor2');
 const divCor3 = document.getElementById('cor3');
 const divCor4 = document.getElementById('cor4');
+const quadroPixels = document.getElementById('pixel-board');
 
 // console.log(secaoPaletaCores);
 // console.log(divPaletaCores);
 // console.log(botaoCores);
 // console.log(divCor2);
-
 
 function geradorDeCores() {
   const cor2 = parseInt(Math.random() * 255);
@@ -23,23 +23,34 @@ function coresAleatorias() {
   for (let index = 1; index < divPaletaCores.length; index += 1) {
     divPaletaCores[index].style.backgroundColor = geradorDeCores();
   }
-  let paletaCompleta = {
+  const paletaCompleta = {
     paleta2: divCor2.style.backgroundColor,
     paleta3: divCor3.style.backgroundColor,
-    paleta4: divCor4.style.backgroundColor
-   };
-  //console.log(paletaCompleta);
+    paleta4: divCor4.style.backgroundColor,
+  };
+  // console.log(paletaCompleta);
   localStorage.setItem('colorPalette', JSON.stringify(paletaCompleta));
 }
 
 const botaoDoClick = botaoCores.addEventListener('click', coresAleatorias);
-let paletaSalva = JSON.parse(localStorage.getItem('colorPalette'));
-//console.log (paletaSalva);
+const paletaSalva = JSON.parse(localStorage.getItem('colorPalette'));
+// console.log (paletaSalva);
 if (paletaSalva !== null) {
   divCor2.style.backgroundColor = paletaSalva.paleta2;
   divCor3.style.backgroundColor = paletaSalva.paleta3;
   divCor4.style.backgroundColor = paletaSalva.paleta4;
 }
 
+let secaoPixels; // Por que dá undefined quando coloco dentro do for?
+for (let index2 = 1; index2 < 5; index2 += 1) {
+  secaoPixels = document.createElement('section');
+  secaoPixels.className = 'coluna-pixels';
+  quadroPixels.appendChild(secaoPixels);
+  for (let index3 = 1; index3 < 6; index3 += 1) {
+   let divPixels = document.createElement('div');
+   divPixels.className = 'pixel';
+   secaoPixels.appendChild(divPixels);
+  }
+}
 
-
+console.log (secaoPixels)
